1334f49a3a24be43ead014107358e8f28e636ec2
quang.tran <quang.tran@alta.com.vn>
2021-11-30 18:01:32 +0700

    adding account management

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AccountManagement.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountManagement.Data
+{
+    public class ApiDbContext : IdentityDbContext
+    {
+        public virtual DbSet<Account> Items { get; set; }
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
+
+
+
---
+++ Models/Account.cs
@@ -0,0 +1,13 @@
+﻿namespace AccountManagement.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Avatar { get; set; }
+        public DateTime LastAccess { get; set; }
+        
+    }
+}
